0e22766ab4a5b8ca24162670a5c9f4e9e1b5bf5a
Adyb Fadel <adyb.fadel@pyxintel.com>
2022-08-30 17:23:29 +0200

    Update

--- class8/example/js/menu.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-  document.getElementById('menu').classList.add('open');
-  document.getElementById('menu-backdrop').classList.add('open');
-}
-
-function closeMenu() {
-  document.getElementById('menu').classList.remove('open');
-  document.getElementById('menu-backdrop').classList.remove('open');
-}
